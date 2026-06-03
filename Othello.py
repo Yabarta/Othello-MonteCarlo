564fdec50397fde5cec8c592db35891335600663
Yabarta <perezsornipablo@gmail.com>
2026-06-03 23:46:03 +0200

    modal win

--- Othello.py
+++ Othello.py
@@ -25,6 +25,12 @@ class Othello:
             self.nn_model = load_model('modelOthello.keras')
         except:
             self.nn_model = None
+        self.gameOver = False
+        self.whiteCount = 0
+        self.blackCount = 0
+        self.resultText = ''
+        self.font = pygame.font.SysFont(None, 36)
+        self.modalFont = pygame.font.SysFont(None, 28)
 
     def run(self):
         while self.RUN:
@@ -36,42 +42,66 @@ class Othello:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.RUN = False
-            
+        
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 3:
                     self.grid.printGameLogicBoard()
 
-
                 if event.button == 1:
-                    x, y = pygame.mouse.get_pos()
-                    x, y = (x - 80) // 80, (y - 80) // 80
+                    mx, my = pygame.mouse.get_pos()
+                    if self.gameOver:
+                        modalW, modalH = 400, 250
+                        modalX = (800 - modalW) // 2
+                        modalY = (800 - modalH) // 2
+                        btnW, btnH = 140, 40
+                        playRect = pygame.Rect(modalX + 40, modalY + modalH - 70, btnW, btnH)
+                        quitRect = pygame.Rect(modalX + modalW - 40 - btnW, modalY + modalH - 70, btnW, btnH)
+                        if playRect.collidepoint((mx, my)):
+                            self.reset_game()
+                        elif quitRect.collidepoint((mx, my)):
+                            self.RUN = False
+                        continue
+
+                    x, y = (mx - 80) // 80, (my - 80) // 80
                     validCells = self.grid.findAvailMoves(self.grid.gridLogic, self.currentPlayer)
                     if not validCells:
                         pass
-
                     else:
-                        if (y,x) in validCells:
+                        if (y, x) in validCells:
                             self.grid.insertToken(self.grid.gridLogic, self.currentPlayer, y, x)
-                            swappableTiles = self.grid.swappableTiles(y,x ,self.grid.gridLogic , self.currentPlayer)
+                            swappableTiles = self.grid.swappableTiles(y, x, self.grid.gridLogic, self.currentPlayer)
                             for tile in swappableTiles:
                                 self.grid.insertToken(self.grid.gridLogic, self.currentPlayer, tile[0], tile[1])
                             if self.currentPlayer == white:
                                 self.currentPlayer = black
-                            else: 
+                            else:
                                 self.currentPlayer = white
 
     def update(self):
-        
+        if self.gameOver:
+            return
+
         validCells = self.grid.findAvailMoves(self.grid.gridLogic, self.currentPlayer)
-        
+
         if not validCells:
             if self.currentPlayer == white:
                 nextPlayer = black
-            else: 
+            else:
                 nextPlayer = white
             if not self.grid.findAvailMoves(self.grid.gridLogic, nextPlayer):
-                print("Fin del juego")
-                self.RUN = False
+                
+                whites = sum(row.count(white) for row in self.grid.gridLogic)
+                blacks = sum(row.count(black) for row in self.grid.gridLogic)
+                self.whiteCount = whites
+                self.blackCount = blacks
+                if whites > blacks:
+                    self.resultText = 'You win!'
+                elif whites < blacks:
+                    self.resultText = 'You lose.'
+                else:
+                    self.resultText = 'Draw.'
+                self.gameOver = True
+                print('Fin del juego')
                 return
             else:
                 print(f"El jugador {'Blanco' if self.currentPlayer == white else 'Negro'} no puede mover. Pasa turno.")
@@ -102,4 +132,42 @@ class Othello:
     def draw(self):
         self.screen.fill((0, 0, 0))
         self.grid.drawGrid(self.screen)
+        
+        if self.gameOver:
+            modalW, modalH = 400, 250
+            modalX = (800 - modalW) // 2
+            modalY = (800 - modalH) // 2
+            overlay = pygame.Surface((modalW, modalH))
+            overlay.set_alpha(230)
+            overlay.fill((200, 200, 200))
+            self.screen.blit(overlay, (modalX, modalY))
+            # title
+            title = self.font.render('Game Over', True, (10,10,10))
+            self.screen.blit(title, title.get_rect(center=(modalX + modalW//2, modalY + 30)))
+            # counts
+            counts_text = f'White: {self.whiteCount}    Black: {self.blackCount}'
+            counts = self.modalFont.render(counts_text, True, (10,10,10))
+            self.screen.blit(counts, counts.get_rect(center=(modalX + modalW//2, modalY + 90)))
+            # result
+            res = self.modalFont.render(self.resultText, True, (10,10,10))
+            self.screen.blit(res, res.get_rect(center=(modalX + modalW//2, modalY + 130)))
+            # buttons
+            btnW, btnH = 140, 40
+            playRect = pygame.Rect(modalX + 40, modalY + modalH - 70, btnW, btnH)
+            quitRect = pygame.Rect(modalX + modalW - 40 - btnW, modalY + modalH - 70, btnW, btnH)
+            pygame.draw.rect(self.screen, (100,200,100), playRect)
+            pygame.draw.rect(self.screen, (200,100,100), quitRect)
+            playS = self.modalFont.render('Play Again', True, (0,0,0))
+            quitS = self.modalFont.render('Quit', True, (0,0,0))
+            self.screen.blit(playS, playS.get_rect(center=playRect.center))
+            self.screen.blit(quitS, quitS.get_rect(center=quitRect.center))
+
         pygame.display.update()
+
+    def reset_game(self):
+        self.grid = Grid(self.rows, self.columns, (80, 80), self)
+        self.currentPlayer = white
+        self.gameOver = False
+        self.whiteCount = 0
+        self.blackCount = 0
+        self.resultText = ''
